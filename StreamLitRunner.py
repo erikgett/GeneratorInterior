cd16dedd93a95e66c264c5f6caadd4a3d6c51f26
erikgett <erikgette63@gmail.com>
2023-06-14 12:11:19 +0500

    some refator

--- StreamLitRunner.py
+++ StreamLitRunner.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.write("# Получение дизайна планировки по изображению")
 
-
+uploaded_files = st.file_uploader("Загрузите изображение",
+                                  accept_multiple_files=False)
